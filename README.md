2b7d86dd3db3b0b03083282324ecb452f2392c97
biendo <aime@awelty.com>
2017-10-18 16:57:02 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Product Name
+> Collection related classes for the Hubrisjs javascript framework.
+
+Some command classes: ArrayMap, SparseArray, Bst, RingBuffer, Queue etc...
+
+## Installation
+
+Node:
+
+```sh
+npm install hjs-codec --save
+```
+
+## Meta
+
+Aime – abiendo@gmail.com
+
+Distributed under the MIT license. See ``LICENSE`` for more information.
---
+++ index.js
@@ -0,0 +1,76 @@
+/** @babel */
+import {
+    ArrayMap,
+    SparseArray} from './lib/array';
+import {
+    BSTNode,
+    BST} from './lib/bst';
+import {
+    RingBuffer} from './lib/buffer';
+import {
+    CircularIterator,
+    Comparator,
+    Enumeration,
+    Iterator} from './lib/iterator';
+import {
+    LinkedList,
+    LinkedListIterator,
+    LinkedNode} from './lib/list';
+import {
+    AbstractMap
+} from './lib/map';
+import {
+    DEFAULT_QUEUE_CAPACITY,
+    AbstractQueue,
+    Queue} from './lib/queue';
+import {
+    Enumerator,
+    HashtableEntry,
+    Hashtable,
+    HashMapEntry,
+    HashMap} from './lib/table';
+import {
+    TraversalAction,
+    TreeNodeIterator,
+    TreeNode,
+    MultiTreeNode,
+    ArrayMultiTreeNode,
+    LinkedMultiTreeNode} from './lib/tree';
+
+export {
+    ArrayMap,
+    SparseArray,
+
+    BSTNode,
+    BST,
+
+    RingBuffer,
+
+    CircularIterator,
+    Comparator,
+    Enumeration,
+    Iterator,
+
+    LinkedList,
+    LinkedListIterator,
+    LinkedNode,
+
+    AbstractMap,
+
+    DEFAULT_QUEUE_CAPACITY,
+    AbstractQueue,
+    Queue,
+
+    Enumerator,
+    HashtableEntry,
+    Hashtable,
+    HashMapEntry,
+    HashMap,
+
+    TraversalAction,
+    TreeNodeIterator,
+    TreeNode,
+    MultiTreeNode,
+    ArrayMultiTreeNode,
+    LinkedMultiTreeNode
+}
